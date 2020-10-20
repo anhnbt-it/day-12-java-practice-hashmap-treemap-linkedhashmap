67ef62772589bebb3e6c451435f2211966314ba7
anhnbt-it <anhnbt.it@gmail.com>
2020-10-20 13:46:52 +0700

    test: TreeMap

--- src/TestMap.java
+++ src/TestMap.java
@@ -1,5 +1,9 @@
+import sun.reflect.generics.tree.Tree;
+
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Created by IntelliJ IDEA.
@@ -9,11 +13,25 @@ import java.util.Map;
  */
 public class TestMap {
     public static void main(String[] args) {
+        // Test HashMap
         Map<Integer, String> hashMap = new HashMap<>();
-        hashMap.put(1, "Khoai Tay");
+        hashMap.put(5, "Khoai Tay");
         hashMap.put(2, "Rose");
-        hashMap.put(3, "AnhNBT");
+        hashMap.put(10, "AnhNBT");
         System.out.println("Display entries in HashMap");
         System.out.println(hashMap + "\n");
+
+        // Test TreeMap
+        Map<Integer, String> treeMap = new TreeMap<>(hashMap);
+        System.out.println("Display entries in ascending order of key");
+        System.out.println(treeMap);
+
+        Map<Integer, String> linkedHashMap = new LinkedHashMap<>(16, 0.75f, true);
+        linkedHashMap.put(30, "Smith");
+        linkedHashMap.put(31, "Anderson");
+        linkedHashMap.put(29, "Lewis");
+        linkedHashMap.put(29, "Cook");
+        System.out.println("\nThe age for Lewis is " + linkedHashMap.get("Lewis"));
+
     }
 }
